988ede42bf83c0d619e27e59d22349687a5dd8cd
Andrii Kopp <kopp93@gmail.com>
2017-09-21 18:52:36 +0300

    Add constants interface.

---
+++ jms-2.0/src/ua/khpi/constants/Constants.java
@@ -0,0 +1,15 @@
+package ua.khpi.constants;
+
+public interface Constants {
+	public static final String QUEUE = "jms/Lab03";
+	
+	public static final String SENDER = "sender";
+	public static final String RECEIVER = "receiver";
+
+	public static final String SERVICE = "service";
+	public static final String TEXT = "text";
+
+	public static final String ERROR = "error";
+
+	public static final String HOME = "index.jsp";
+}
